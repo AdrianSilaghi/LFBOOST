fbfdffe1f40b72ad0122279b265abc7c57149bf1
AdrianSilaghi <silaghi.adrian95@gmail.com>
2018-07-26 22:37:56 +0300

    Message

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -35,7 +35,7 @@ const app = new Vue({
         
     }
 });
-//push this pleasesssss
+//push 
 $(document).ready(function(){
     var button = document.querySelector('#markasComplete');
     button.addEventListener('click',function(){
