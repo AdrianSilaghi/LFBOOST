476b6c782884f42afe81b929cda2367d23b0e1f9
AdrianSilaghi <silaghi.adrian95@gmail.com>
2018-07-26 13:23:29 +0300

    new

--- resources/views/outsoon.blade.php
+++ resources/views/outsoon.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.payments')
 @section('content')
-
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                  <h1 class="display-3">The first marketplace for boosting services!</h1>
+                  <h1 class="display-2">Comming out soon!</h1>
+                  <p class="lead">Be Ready!</p>
+                </div>
+              </div>
+              <hr>
+        </div>
+    </div>
+</div>
 
 @endsection
