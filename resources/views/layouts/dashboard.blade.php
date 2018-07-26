291a6e8aa7bdd2dff5e1de9d07a51ebb01cc15c5
AdrianSilaghi <silaghi.adrian95@gmail.com>
2018-07-26 13:30:37 +0300

    added a few things

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="userId" content="{{ Auth::check() ? Auth::user()->id : '' }}">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{ csrf_field() }}
+    <title>LFBOOST</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('css/select2.css')}}">
+    <link rel="stylesheet" href="{{asset('css/algolia.css')}}">
+    <link rel="stylesheet" href="{{asset('css/smart_wizard.css')}}">
+    <link rel="stylesheet" href="{{asset('css/smart_wizard_theme_arrows.css')}}">
+    <link rel="stylesheet" href="{{asset('css/dropzone.css')}}">
+    <link rel="stylesheet" href="{{asset('css/parsley.css')}}">
+    <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+    <link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/bootstrap-tagsinput.css')}}">
+    <link rel="stylesheet" href="{{asset('css/fontawesome-stars.css')}}">
+</head>
+
+<body>
+    <div id="app">
+        @include('_includes.nav.main')
+        @include('_includes.nav.dashboardmenu')
+        @include('_includes.messages')
+        <div class="container">
+            <div class="row justify-content-center">
+                
+                
+                @yield('content')
+                
+        </div>
+        </div>
+        @include('_includes.nav.footer')
+    </div>
+</body>
+    <!-- Scripts -->
+    <!-- development version, includes helpful console warnings -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
+    
+    <script src="{{asset('js/select2.js')}}"></script>
+    <script src="{{asset('js/algolia.js')}}"></script>
+    <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
+    <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
+    <script src="{{asset('js/dropzone.js')}}"></script>
+    <script src="{{asset('js/jquery.smartWizard.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.min.js"></script>
+    <script src="{{asset('js/bootstrap-tagsinput.js')}}"></script>
+    <script src="{{asset('js/jquery.barrating.min.js')}}"></script>
+    
+</html>
--- resources/views/outsoon.blade.php
+++ resources/views/outsoon.blade.php
@@ -7,7 +7,7 @@
                 <div class="container">
                   <h1 class="display-3">The first marketplace for boosting services!</h1>
                   <h1 class="display-2">Comming out soon!</h1>
-                  <p class="lead">Be Ready!</p>
+                  
                 </div>
               </div>
               <hr>
