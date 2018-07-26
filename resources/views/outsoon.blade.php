75b4b31676a9dcd2800773e32ae62f99b65b49ff
AdrianSilaghi <silaghi.adrian95@gmail.com>
2018-07-26 13:24:15 +0300

    there are tho

--- resources/views/outsoon.blade.php
+++ resources/views/outsoon.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col">
+        <div class="col text-center">
             <div class="jumbotron jumbotron-fluid">
                 <div class="container">
                   <h1 class="display-3">The first marketplace for boosting services!</h1>
