3b826a5f2e3c3b415763878b10cbc957ce048131
AdrianSilaghi <silaghi.adrian95@gmail.com>
2018-07-26 13:32:10 +0300

    there are tho

--- resources/views/outsoon.blade.php
+++ resources/views/outsoon.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.payments')
 @section('content')
-<div class="container">
+<div class="container m-t-100">
     <div class="row">
         <div class="col text-center">
             <div class="jumbotron jumbotron-fluid">
                 <div class="container">
                   <h1 class="display-3">The first marketplace for boosting services!</h1>
-                  <h1 class="display-2">Comming out soon!</h1>
+                  <h1 class="display-4">Comming out soon!</h1>
                   
                 </div>
               </div>
