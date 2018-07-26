dc6c8855b27d85c6f45b24a6ebe4725098665bef
AdrianSilaghi <silaghi.adrian95@gmail.com>
2018-07-26 22:28:28 +0300

    go agian

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -35,7 +35,7 @@ const app = new Vue({
         
     }
 });
-//push this pleases
+//push this pleasess
 $(document).ready(function(){
     var button = document.querySelector('#markasComplete');
     button.addEventListener('click',function(){
