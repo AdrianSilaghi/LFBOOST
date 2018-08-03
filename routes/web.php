c9482b0de7c5d81fc9ec066b166bc45616e05e9a
AdrianSilaghi <silaghi.adrian95@gmail.com>
2018-08-03 13:56:14 +0300

    remove routes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 use Illuminate\Support\Facades\Mail;
-Auth::routes();
+
 
 Route::get('/contactsupport/api/getFirstQuestions','ContactSupportController@getFirstQuestions')->middleware('auth');
 
