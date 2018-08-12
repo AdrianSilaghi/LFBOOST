74bb9f38cc6e0b4948bacbe0826f0b82d2297d67
AdrianSilaghi <silaghi.adrian95@gmail.com>
2018-08-12 19:59:36 +0300

    push

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -80,7 +80,7 @@ class UsersController extends Controller
     public function validateForm(Request $request){
         $this->validate($request,[
             'name' => 'required|string|max:255|unique:users|regex:/^[a-zA-Z0-9\s]*$/',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|indisposable|max:255|unique:users',
             'password' => 'required|string|min:6',
 
         ]);
