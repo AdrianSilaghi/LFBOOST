f715595da0285d7ca1405dedd7fc749f596153e7
adrian <silaghi.adrian95@gmail.com>
2018-09-02 19:15:34 +0300

    fixed payment added a few stuff

---
+++ app/Mail/NewOrderMail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NewOrderMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $order;
+    public $user;
+    public function __construct($user,$order)
+    {
+        $this->user = $user;
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('neworderemail')
+            ->subject('You have a new order')
+            ->from('no-reply@lfboost.com')
+            ->with([
+                'user'=>$this->user,
+                'order'=>$this->order
+            ]);
+    }
+}
